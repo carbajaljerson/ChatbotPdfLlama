b70d5d6e8b7582d8b22449a90aa34e7f59e4b243
Jerson Carbajal <jerson182@hotmail.com>
2023-10-09 02:45:34 -0500

    fixing app.py

--- constants.py
+++
@@ -1,9 +0,0 @@
-import os 
-from chromadb.config import Settings 
-
-#Define Chroma settings
-CHROMA_SETTINGS = Settings(
-        chroma_db_impl='duckdb+parquet',
-        persist_directory='db',
-        anonymized_telemetry=False
-)
--- ingest.py
+++ ingest.py
@@ -3,17 +3,36 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import SentenceTransformerEmbeddings 
 from langchain.vectorstores import Chroma 
 import os 
-from constants import CHROMA_SETTINGS
 
-persist_directory = "db"
+
+from langchain.document_loaders import (
+    DirectoryLoader,
+    PyPDFLoader,
+    TextLoader,
+    UnstructuredMarkdownLoader,
+)
+
+
+ABS_PATH: str = os.path.dirname(os.path.abspath(__file__))
+DB_DIR: str = os.path.join(ABS_PATH, "db")
 
 def main():
-    for root, dirs, files in os.walk("docs"):
-        for file in files:
-            if file.endswith(".pdf"):
-                print(file)
-                loader = PyPDFLoader(os.path.join(root, file))
-    documents = loader.load()
+    
+    # Initialize loaders for different file types
+    pdf_loader = DirectoryLoader("data/", glob="**/*.pdf", loader_cls=PyPDFLoader)
+    markdown_loader = DirectoryLoader(
+        "data/", glob="**/*.md", loader_cls=UnstructuredMarkdownLoader
+    )
+    text_loader = DirectoryLoader("data/", glob="**/*.txt", loader_cls=TextLoader)
+
+    all_loaders = [pdf_loader, markdown_loader, text_loader]
+
+    # Load documents from all loaders
+    documents = []
+    for loader in all_loaders:
+        documents.extend(loader.load())
+    
+    
     print("splitting into chunks")
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     texts = text_splitter.split_documents(documents)
@@ -22,11 +41,11 @@ def main():
     embeddings = SentenceTransformerEmbeddings(model_name="./model/all-MiniLM-L6-v2")
     #create vector store here
     print(f"Creating embeddings. May take some minutes...")
-    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
+    db = Chroma.from_documents(texts, embeddings, persist_directory=DB_DIR)
     db.persist()
     db=None 
 
-    print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
+    print(f"Ingestion complete! You can now run to query your documents")
 
 if __name__ == "__main__":
     main()
